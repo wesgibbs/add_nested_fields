7f8b13c0ad3f7ae98494fafd9f23048052b846d0
Paul Elliott & Wes Gibbs <dev+paulelliott+wgibbs@hashrocket.com>
2009-10-15 17:09:56 -0400

    Convert to jQuery

--- lib/add_nested_fields.rb
+++ lib/add_nested_fields.rb
@@ -1,37 +1,39 @@
 module AddNestedFields
-	module ViewHelper
-		
-		def add_nested_fields_for(form_builder, field, dom_id, *args)
-			options = {
-				:partial => field.to_s.singularize,
-				:label => "Add #{field.to_s.singularize.titleize}",
-				:object => field.to_s.classify.constantize.new
-			}.merge(args.extract_options!)
+  module ViewHelper
 
-			link_to_function("#{options[:label]}") do |page|
-				form_builder.fields_for field, options[:object] , :child_index => 'NEW_RECORD' do |f|
-			  	html = render(:partial => options[:partial], :locals => { :f => f })
-					page << "$('#{dom_id}').insert({ bottom: '#{escape_javascript(html)}'.replace(/NEW_RECORD/g, new Date().getTime()) });"
-				end
-			end
-		end
+    def add_nested_fields_for(form_builder, field, selector, *args)
+      options = {
+        :partial => field.to_s.singularize,
+        :label => "Add #{field.to_s.singularize.titleize}",
+        :label_class => "",
+        :object => field.to_s.classify.constantize.new
+      }.merge(args.extract_options!)
 
-		def remove_nested_fields_for(form_builder, class_id, *args)
-			options = {
-				:label => 'remove'
-			}.merge(args.extract_options!)
-			confirm = "if (confirm('Are you sure you would like to delete this #{form_builder.object.class.to_s.underscore.humanize.downcase}?'))"
-			if form_builder.object.new_record?
-				link_to_function(options[:label]) do |page|
-					page << "#{confirm} $(this).up('.#{class_id}').remove()"
-				end
-			else
-				form_builder.hidden_field( :_delete, :value => "0") + link_to_function(options[:label]) do |page|
-					page << "#{confirm} $(this).up('.#{class_id}').hide();$(this).previous().value = 1"
-				end
-			end
-		end
-		
-	end
+      link_to_function("#{options[:label]}", :class => options[:label_class]) do |page|
+        form_builder.fields_for field, options[:object] , :child_index => 'NEW_RECORD' do |f|
+          html = render(:partial => options[:partial], :locals => { :f => f })
+          page << "$('#{selector}').append('#{escape_javascript(html)}'.replace(/NEW_RECORD/g, new Date().getTime())).trigger('nested.field.added');"
+        end
+      end
+    end
+
+    def remove_nested_fields_for(form_builder, selector, *args)
+      options = {
+        :label => 'remove',
+        :label_class => ''
+      }.merge(args.extract_options!)
+      confirm = "if (confirm('Are you sure you would like to delete this #{form_builder.object.class.to_s.underscore.humanize.downcase}?'))"
+      if form_builder.object.new_record?
+        link_to_function(options[:label], :class => options[:label_class]) do |page|
+          page << "#{confirm} $(this).parents('#{selector}').hide().trigger('nested.field.removed').remove();"
+        end
+      else
+        form_builder.hidden_field( :_delete, :value => "0") + link_to_function(options[:label], :class => options[:label_class]) do |page|
+          page << "#{confirm} $(this).parents('#{selector}').hide().trigger('nested.field.removed');$(this).prev(':hidden').val(1)"
+        end
+      end
+    end
+
+  end
 end
 ActionView::Base.send(:include, AddNestedFields::ViewHelper)
